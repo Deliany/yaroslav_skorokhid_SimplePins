f29cf3786d281af9d74dc8ac77a5d757ef7316c7
Yaroslav Skorokhid <radomba@gmail.com>
2016-10-15 15:43:54 +0300

    Initial Commit

---
+++ yaroslav_skorokhid_SimplePins/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  yaroslav_skorokhid_SimplePins
+//
+//  Created by Yaroslav Skorokhid on 10/15/16.
+//  Copyright © 2016 CollateralBeauty. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
